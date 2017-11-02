f8c86700bab730ca75db9715568d2bdc03a420f7
Parv Kumar <parvathikumar92@gmail.com>
2017-11-02 00:09:48 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+let index = 0
+
+function init(e) {
   // Write your JavaScript code inside the init() function
+  let select = document.body.addEventListener('keydown')
+
 
 }
